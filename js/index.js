bf0641a74e1da115bea6eecde543b31667572f82
venanciotayna <venancio_tayna@outlook.com>
2022-04-19 19:03:42 -0300

    commit inicial

---
+++ js/index.js
@@ -0,0 +1,27 @@
+function soma(numero1, numero2){
+    let soma = numero1 + numero2;
+    return 'A soma dos dois números é: ' + soma;
+}
+
+console.log(soma(10,20))
+
+function subtracao(numero1, numero2){
+    let subtracao = numero1 - numero2;
+    return 'A subtração dos dois números é: ' + subtracao;
+}
+
+console.log(subtracao(10,20))
+
+function multiplicacao(numero1, numero2){
+    let multiplicacao = numero1 * numero2;
+    return 'A multiplicacao dos dois números é: ' + multiplicacao;
+}
+
+console.log(multiplicacao(10,20))
+
+function divisao(numero1, numero2){
+    let divisao = numero1 / numero2;
+    return 'A divisao dos dois números é: ' + divisao;
+}
+
+console.log(divisao(10,20))
